0fcfa9477c255c03125462f0e6cd2e0220de114a
Siva Prakash <prakashdeveloper@outlook.in>
2024-02-21 09:15:13 +0530

    Integrated Supabase

--- client/src/utils/supabase.ts
+++ client/src/utils/supabase.ts
@@ -1,7 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://vpaznvslotfbdqwptizp.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwYXpudnNsb3RmYmRxd3B0aXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MjIyOTMsImV4cCI6MjAyMzk5ODI5M30.f6PokQiuDqVtTRM0yZqJWvuWtbJBbApxIWfHpJXPyD0';
-
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
